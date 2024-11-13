d159b5daee8f69708c438bf535ebf0f15d04ce13
Lasse Bøgeskov-Jensen <lbj@kuatro-group.com>
2024-11-13 14:03:22 +0100

    added PostLikeTrigger migration

---
+++ keasocial/Migrations/20241113123810_AddPostLikeTrigger.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace keasocial.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPostLikeTrigger : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+        CREATE TRIGGER increment_like_count
+        AFTER INSERT ON PostLikes
+        FOR EACH ROW
+        BEGIN
+            UPDATE Posts
+            SET LikeCount = LikeCount + 1
+            WHERE PostId = NEW.PostId;
+        END;
+    ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER IF EXISTS increment_like_count;");
+        }
+    }
+}
