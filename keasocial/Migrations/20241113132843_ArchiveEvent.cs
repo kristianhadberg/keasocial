a2722832f307ef57c9acd0d3c6247dbe3dec28d2
kris52h6 <kris52h6@stud.kea.dk>
2024-11-13 14:40:54 +0100

    feat: add recurring events in the database

---
+++ keasocial/Migrations/20241113132843_ArchiveEvent.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace keasocial.Migrations
+{
+    /// <inheritdoc />
+    public partial class ArchiveEvent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                    CREATE TABLE `ArchivedLecture` (
+                  `ArchivedLectureId` int NOT NULL AUTO_INCREMENT,
+                  `LectureTitle` longtext NOT NULL,
+                  `LectureDescription` longtext NOT NULL,
+                  `LectureDate` datetime NOT NULL,
+                  `LectureTime` time NOT NULL,
+                  `LectureId` int NOT NULL,
+                  PRIMARY KEY (`ArchivedLectureId`)
+                ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+            ");
+
+            migrationBuilder.Sql(@"
+                DROP EVENT IF EXISTS delete_old_lectures;
+
+                CREATE EVENT delete_old_lectures
+                ON schedule
+	                EVERY 1 YEAR
+                    STARTS '2025-01-01'
+                DO BEGIN
+	                INSERT INTO ArchivedLecture (LectureId, LectureTitle, LectureDescription, LectureDate, LectureTime)
+                    SELECT LectureId, LectureTitle, LectureDescription, LectureDate, LectureTime
+                    FROM Lectures
+                    WHERE LectureDate < NOW() - INTERVAL 1 YEAR;
+                    
+                    DELETE FROM Lectures
+	                WHERE LectureDate < NOW() - INTERVAL 1 YEAR;
+
+                END;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
